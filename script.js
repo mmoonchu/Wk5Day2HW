799e5a2405546b2419ddf39660baeceb219ac2d2
Moon Chu <mmoonchu2@gmail.com>
2023-11-05 23:47:55 -0500

    Completed section 2

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 // 1.3
 mainEl.classList.add('flex-ctr');
 // 2.0
+const topMenuEl = document.querySelector('#top-menu');
+// 2.1
+topMenuEl.style.height = '100%';
+// 2.2
+topMenuEl.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--top-menu-bg');
+// 2.3
+topMenuEl.classList.add('flex-around');
