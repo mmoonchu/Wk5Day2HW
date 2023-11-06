08f15e10aa5bde3de2b23d69b57de6ad1a1a576c
Moon Chu <mmoonchu2@gmail.com>
2023-11-05 23:45:41 -0500

    Completed section 1

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// 1.0
 const mainEl = document.querySelector('main');
+// 1.1
 mainEl.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--main-bg');
+// 1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+// 1.3
+mainEl.classList.add('flex-ctr');
+// 2.0
